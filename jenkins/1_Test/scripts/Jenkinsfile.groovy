78e8053880288b74f6d23e5babc2cfac3c70fdf2
gdha <gratien.dhaese@gmail.com>
2023-10-27 15:51:13 +0200

    yet another trial

--- jenkins/1_Test/scripts/Jenkinsfile.groovy
+++ jenkins/1_Test/scripts/Jenkinsfile.groovy
@@ -1,8 +1,13 @@
+#!/usr/bin/env groovy
+
+// see https://wilsonmar.github.io/jenkins2-pipeline/ for an example
+
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-import groovy.text.SimpleTemplateEngine
 import org.apache.commons.io.FileUtils
+import hudson.EnvVars
 import hudson.model.*
 import jenkins.model.Jenkins
+import groovy.text.SimpleTemplateEngine
 import groovy.json.*
 
 // Inputs
@@ -12,25 +17,42 @@ def yyyyMM = java.time.LocalDate.now().plusMonths(1).format(java.time.format.Dat
 def shEx = "set -ex"
 
 // Paths
-def workspace = sh returnStdout: true, script: 'realpath ${WORKSPACE}/workspace'
-def yyyyMMWS = "${workspace}/${yyyyMM}"
 
-ws("${yyyyMMWS}/ws") {
-  withEnv(["shEx=${shEx}", "PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/usr/bin/python:/usr/local/bin/aws"]) {
-    try {
-      /* Stage: Init
-       * Get list of all supported bundles
-       * Get list of default bundles
-       */
-
-stage('Init') {
-        // build name/description
-            sh script: """
-              $shEx
-              # Clear state
-              cd /app/yumreposerver; python3 lib/certState.py --clear --jira $jira
-            """
-      } // Stage:Init
-   } // try
-  } // withEnv
-} // ws("${yyyyMMWS}/ws")
+try {
+node {
+stage '\u2776 Stage 1'
+echo "\u2600 BUILD_URL=${env.BUILD_URL}"
+ 
+def workspace = pwd()
+echo "\u2600 workspace=${workspace}"
+ 
+stage '\u2777 Stage 2'
+} // node
+} // try end
+catch (exc) {
+/*
+ err = caughtError
+ currentBuild.result = "FAILURE"
+ String recipient = 'infra@lists.jenkins-ci.org'
+ mail subject: "${env.JOB_NAME} (${env.BUILD_NUMBER}) failed",
+         body: "It appears that ${env.BUILD_URL} is failing, somebody should do something about that",
+           to: recipient,
+      replyTo: recipient,
+ from: 'noreply@ci.jenkins.io'
+*/
+} finally {
+  
+ (currentBuild.result != "ABORTED") && node("master") {
+     // Send e-mail notifications for failed or unstable builds.
+     // currentBuild.result must be non-null for this step to work.
+     step([$class: 'Mailer',
+        notifyEveryUnstableBuild: true,
+        recipients: "${email_to}",
+        sendToIndividuals: true])
+ }
+ 
+ // Must re-throw exception to propagate error:
+ if (err) {
+     throw err
+ }
+}
